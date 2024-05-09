310f2ffb2755fe59bb13c474a13f5773426fff9c
beel28 <belengames963@gmail.com>
2024-05-09 11:57:09 +0200

    controller pag principal cargado

---
+++ Downloads/IPC_FXMLCore-master (2)/trabajo_ipc/src/resources/fxml/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package resources.fxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Belén Rodríguez
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button boton_gastos;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void pulsarGastos(ActionEvent event) {
+    }
+    
+}
